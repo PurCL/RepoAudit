c6d60d069706edaa48eca8dbda5ca6948d5690cf
wcphkust <cwangch@connect.ust.hk>
2025-03-14 03:40:10 -0400

    update scripts

---
+++ src/dfscan_extract.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+LANGUAGE=Cpp
+BUG_TYPE=NPD
+PROJECT_NAME=sofa-pbrpc
+
+# create the directory for the result
+if [ ! -d ../result/extract/$BUG_TYPE/${LANGUAGE}_${PROJECT_NAME} ]; then
+  mkdir -p ../result/extract/$BUG_TYPE/${LANGUAGE}_${PROJECT_NAME}
+fi
+
+python3 -m tstool.src_extractor.${LANGUAGE}.${LANGUAGE}_${BUG_TYPE}_extractor \
+  --language $LANGUAGE \
+  --project-path ../benchmark/$LANGUAGE/$PROJECT_NAME \
+  --seed-path ../result/extract/$BUG_TYPE/${LANGUAGE}_${PROJECT_NAME}/seed_result.json
