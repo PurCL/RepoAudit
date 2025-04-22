aea3bf3e67f6222d1a296ea15c7b95b6ec750fe6
chengpeng-wang <15705513022@163.com>
2025-04-22 01:15:55 -0400

    update webui

--- src/ui/web_ui.py
+++ src/ui/web_ui.py
@@ -8,15 +8,14 @@ sys.path.append(str(Path(__file__).resolve().parents[2]))
 
 # Language dictionary
 language_dict = {
-    "C": "c",
     "Cpp": "cpp"
 }
 
 # Base path for results
-BASE_PATH = Path(__file__).resolve().parents[3]
+BASE_PATH = Path(__file__).resolve().parents[2]
 
 # Function to get results
-def get_results(language="C", scanner="DFscan", model="claude-3.7", bug_type="NPD") -> list:
+def get_results(language="C", scanner="bugscan", model="claude-3.7", bug_type="NPD") -> list:
     result_dir = Path(f"{BASE_PATH}/result/{scanner}-{model}/{bug_type}")
     if not result_dir.exists():
         return []
@@ -50,21 +49,29 @@ def display_results():
     # 1. Scanner Selection
     scanner = st.selectbox(
         "Select Scanner",
-        ["DFscan", "bugscan"],
+        ["bugscan", "DFscan"],
         help="Select the scanner"
     )
 
     # 2. Model Selection
     model = st.selectbox(
         "Select Model",
-        ["claude-3.5", "claude-3.7", "gpt-4o", "gpt-4-turbo", "gpt-4o-mini", "deepseek-local", "deepseek-chat", "deepseek-reasoner", "gemini"],
+        ["claude-3.5", "claude-3.7", "o3-mini", "gpt-4o", "gpt-4-turbo", "gpt-4o-mini", "deepseek-local", "deepseek-chat", "deepseek-reasoner", "gemini"],
         help="Select the model"
     )
 
+    scanner_dir = f"{BASE_PATH}/result/{scanner}-{model}"
+    if not Path(scanner_dir).exists():
+        st.info(f"No results available for the {scanner} with {model}.")
+        return
+    bug_types = []
+    for dir in Path(f"{BASE_PATH}/result/{scanner}-{model}").iterdir():
+        if dir.is_dir():
+            bug_types.append(dir.name)
     # 3. Bug Type Selection
     bug_type = st.selectbox(
         "Select Bug Type",
-        ["NPD", "ML", "UAF"],
+        bug_types,
         help="Select the type of bugs to analyze"
     )
 
@@ -78,7 +85,7 @@ def display_results():
 
     # 5. Timestamp Selection only if a project is selected
     if project_name:
-        result_dir = f"{BASE_PATH}/result/{scanner}-{model}/{bug_type}/{language}_{project_name}"
+        result_dir = f"{BASE_PATH}/result/{scanner}-{model}/{bug_type}/{language}--{project_name}"
         if Path(result_dir).exists():
             timestamps = [d.name for d in Path(result_dir).iterdir() if d.is_dir()]
             timestamps.sort(reverse=True)
@@ -89,8 +96,9 @@ def display_results():
             )
         else:
             st.info("Result directory does not exist for the selected project.")
+            return
 
-        result_path = f"{BASE_PATH}/result/{scanner}-{model}/{bug_type}/{language}_{project_name}/{selected_timestamp}/bug_info.json"
+        result_path = f"{BASE_PATH}/result/{scanner}-{model}/{bug_type}/{language}--{project_name}/{selected_timestamp}/detect_info.json"
 
         if Path(result_path).exists():
             col1, col2, col3 = st.columns(3)
@@ -106,7 +114,7 @@ def display_results():
                         # Filter results to keep only TP items
                         tp_results = {}
                         for key, item in all_results.items():
-                            vali_result = item["Vali_human"] if item["Vali_human"] != "" else "False"
+                            vali_result = item["is_human_confirmed_true"] if item["is_human_confirmed_true"] != "unknown" else "False"
                             if vali_result == "True":
                                 tp_results[key] = item
                     st.session_state.analysis_results = tp_results
@@ -120,42 +128,42 @@ def display_results():
     if st.session_state.analysis_results:
         results = st.session_state.analysis_results
         for key, item in results.items():
-            with st.expander(f"{key}"):
-                paths = item["Path"]
-                explanations = item["Explanation"]
+            with st.expander(item["buggy_value"]):
+                paths = item["relevant_functions"]
+                explanations = item["explanation"]
     
                 st.markdown("---")
-                if len(explanations) > 1:
-                    explanations_markdown = "\n".join([f"- {exp.strip()}" for exp in explanations if exp.strip()])
-                else:
-                    explanations_markdown = explanations[0]
+                # if len(explanations) > 1:
+                #     explanations_markdown = "\n".join([f"- {exp.strip()}" for exp in explanations if exp.strip()])
+                # else:
+                #     explanations_markdown = explanations[0]
                 st.markdown("**Explanation:**")
-                st.markdown(explanations_markdown)
-                st.write("**Human Validation Result:**", item["Vali_human"])
+                st.text(explanations)
+                st.write("**Human Validation Result:**", item["is_human_confirmed_true"])
 
                 validation_key = f"validation_{key}"
                 if validation_key not in st.session_state.bug_validations:
-                    st.session_state.bug_validations[validation_key] = item["Vali_human"] if item["Vali_human"] != "" else "Unsure"
+                    st.session_state.bug_validations[validation_key] = item["is_human_confirmed_true"] if item["is_human_confirmed_true"] != "unknown" else "unknown"
                 
                 st.write("**Bug Validation:**")
                 col1, col2 = st.columns(2)
                 with col1:
                     validation = st.radio(
                         "Is this bug true positive or false positive?",
-                        options=["True", "False", "Unsure"],
+                        options=["True", "False", "unknown"],
                         key=validation_key,
                         horizontal=True,
-                        index=["True", "False", "Unsure"].index(st.session_state.bug_validations[validation_key])
+                        index=["True", "False", "unknown"].index(st.session_state.bug_validations[validation_key])
                     )
                 
                     if validation != st.session_state.bug_validations.get(validation_key):
                         st.session_state.bug_validations[validation_key] = validation
                 with col2:
                     if st.button("Save", key=f"save_{key}", use_container_width=True):
-                        item["Vali_human"] = validation
+                        item["is_human_confirmed_true"] = validation
                         with open(result_path, 'r') as f:
                             temp_results = json.load(f)
-                        temp_results[key][0]["Vali_human"] = validation
+                        temp_results[key]["is_human_confirmed_true"] = validation
                         with open(result_path, 'w') as f:
                             json.dump(temp_results, f, indent=4)
 
@@ -169,25 +177,34 @@ def display_results():
                 
                 if st.session_state.show_function.get(key):
                     for path in paths:
-                        source = path["source"]
-                        src_line = path['src_line']
                         function_name = path["function_name"]
                         function_code = path["function_code"]
                         file_name = path["file_name"]
                         st.write(f"**Function: `{function_name}`**")
-                        st.write(f"- Source: `{source}` at line {src_line}")
                         st.write(f"- File: `{file_name}`")
                         st.code(function_code, language=language_dict[language], line_numbers=True)
             
         st.download_button(
             "Download Results",
             data=json.dumps(results, indent=2),
-            file_name="bug_info.json",
+            file_name="detect_info.json",
             mime="application/json"
         )
 
 # Main function to handle navigation
 def main():
+    st.set_page_config(
+        layout="wide",  # Use wide layout instead of centered
+        initial_sidebar_state="expanded"
+    )
+        
+    if 'show_function' not in st.session_state:
+        st.session_state.show_function = {}
+    if 'analysis_results' not in st.session_state:
+        st.session_state.analysis_results = None
+    if 'bug_validations' not in st.session_state:
+        st.session_state.bug_validations = {}
+
     st.sidebar.title("Navigation")
     page = st.sidebar.radio("Go to", ["Home", "Results"])
 
